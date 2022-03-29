434663f6cdba6442f3e4e811b14232f8bf47f49a
chrisfleet1 <85644957+chrisfleet1@users.noreply.github.com>
2022-03-29 22:18:30 +0100

    added basics and styling for contact page, adjusted index styling

---
+++ js/greeting.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening and welcome to my page!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon and welcome to my page!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning and welcome to my page!';
+} else {
+    greeting = 'Welcome to my page!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
